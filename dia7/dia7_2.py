e0adfab53aaea9d8bdd5fd92b21961c279d2488e
Raul <rpublico@gmail.com>
2022-12-13 16:07:13 +0000

    New: dia7 Calcular tamaños directorios

---
+++ dia7/dia7_2.py
@@ -0,0 +1,50 @@
+
+tree = []
+def tree_append(dir, total):
+    tree.append([dir, total])
+
+def ir_a(file, dir) -> int:
+    line = file.readline()
+    total = 0
+    while line:
+        line_items = line.split(" ")
+        # si la linea es igual "$ cd .."
+        if line == "$ cd ..\n":
+            tree_append(dir, total)
+            return total
+            
+        # si la linea comienza es igual a "$ cd"
+        if line.startswith("$ cd"):
+            new_dir = line_items[2].strip()
+            total += ir_a(file, new_dir)
+
+        # si line_items[0] es un numero
+        if line_items[0].isdigit():
+            total += int(line_items[0])
+
+        line = file.readline()
+    
+    tree_append(dir, total)
+    return total
+
+with open('entrada.txt', 'r') as f:
+    f.readline()
+    ir_a(f, 'root')
+    
+    total = 0
+    for item in tree:
+        if item[1] <= 100000:
+            ##* print(item[0], item[1])
+            total += item[1]
+    
+    print(total)
+    diffmax = tree[-1][1]
+    diff = 30000000 - (70000000 - diffmax)
+
+    print(diff, diffmax)
+    
+    for item in tree:
+        if (item[1] > diff) and (item[1] < diffmax):
+            diffmax = item[1]
+    
+    print(diffmax)
