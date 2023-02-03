10bf172d43544b002c401db802c411b5995169e9
Raul <rpublico@gmail.com>
2023-02-03 08:16:24 +0000

    New: dia11 monkeys

---
+++ dia11/__main__.py
@@ -0,0 +1,6 @@
+import dia11
+
+if __name__ == "__main__":
+
+    dia11.dia11_1("./dia11/test.txt")
+    dia11.dia11_2("./dia11/test.txt")
---
+++ dia11/monkey.py
@@ -0,0 +1,17 @@
+# definir una clase mono 
+# atributos: id: int, items:list, operation: lambda, test: int, worried: int, noworried:int
+class Monkey:
+    id: int
+    items: list
+    operation = lambda x: x
+    test: int
+    worried: int
+    noworried: int
+    # constructor
+    def __init__(self, id, items, operation, test, worried, noworried):
+        self.id = id
+        self.items = items
+        self.operation = operation
+        self.test = test
+        self.worried = worried
+        self.noworried = noworried
